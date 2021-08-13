91a0f062e11e1be095e8cb8306e10d446a1ee23c
Son Thanh Vo <sonvo95@outlook.com>
2021-08-13 16:02:29 +0200

    test: add parameterized tests for fv

--- src/1-function/future-value.test.ts
+++ src/1-function/future-value.test.ts
@@ -9,8 +9,22 @@ describe('verify future value calculation', () => {
   });
 
   test('increase in value over time', () => {
-    expect(fv(0.2, 12, 0, 1000)).toBe(-8916.10);
-    expect(fv(0.2, 12, 200, 1000)).toBe(-16832.20);
-    expect(fv(0.2, 12, 200, 0)).toBe(-7916.10);
+    expect(fv(0.2, 12, 0, 1000)).toBe(-8916.1);
+    expect(fv(0.2, 12, 200, 1000)).toBe(-16832.2);
+    expect(fv(0.2, 12, 200, 0)).toBe(-7916.1);
   });
-})
+
+  test.each([
+    [0, 12, 0, 1000, -1000],
+    [0, 12, 200, 1000, -3400],
+    [0, 12, 200, 0, -2400],
+    [0.2, 12, 0, 1000, -8916.1],
+    [0.2, 12, 200, 1000, -16832.2],
+    [0.2, 12, 200, 0, -7916.1],
+  ])(
+    'fv(rate: %d, nper: %d, pmt: %d, pv: %d) = %d',
+    (rate: number, nper: number, pmt: number, pv: number, expected: number) => {
+      expect(fv(rate, nper, pmt, pv)).toBe(expected);
+    }
+  );
+});
